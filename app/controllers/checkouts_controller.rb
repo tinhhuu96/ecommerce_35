c10e5f5753e1c0b6eee76533c859799af6b84269
huutinh <tinhhuu96@gmail.com>
2017-10-19 12:12:18 +0700

    finish send mail customer

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,4 +1,5 @@
 class CheckoutsController < ApplicationController
+  before_action :update_quantity_product, only: [:create]
   def index
     @user = current_user
     return if @user
@@ -8,7 +9,8 @@ class CheckoutsController < ApplicationController
 
   def create
     if update_cart order_params
-      flash[:success] = t "order_success"
+      UserNotifierMailer.send_order_email(current_user, @cart).deliver_now
+      flash[:success] = t "order_success_pls_check_mail"
       session[:cart_id] = nil
     else
       flash[:danger] = t "order_error"
@@ -16,13 +18,35 @@ class CheckoutsController < ApplicationController
     redirect_to root_path
   end
 
+  def load_product product_id
+    @product = Product.find_by id: product_id
+    return if @product
+    flash[:danger] = t "not_found_product"
+    redirect_to cart_path
+  end
+
   private
 
   def order_params
-    params.require(:user).permit :name, :phone, :email, :address, :order_statuses_id
+    params.require(:user).permit :name, :phone, :email, :address
   end
 
   def update_cart params_order
-    @cart.update_attributes params_order
+    if @cart.update_attributes params_order
+      @cart.placed!
+    else
+      flash[:danger] = t "order_error"
+    end
+  end
+
+  def update_quantity_product
+    @cart.order_details.each do |item|
+      load_product item.product_id
+      quantity = @product.quantity - item.quantity
+      unless @product.update_attributes(quantity: quantity)
+        flash[:danger] = "quantity not avalible"
+        redirect_to cart_path
+      end
+    end
   end
 end
